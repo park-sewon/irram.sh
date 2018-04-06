8e2511c57625cddf2c2918ac0bbaf5a89ac696b5
Sewon Park <swelite@kaist.ac.kr>
2018-04-06 22:36:05 +0900

    license added

--- irram.py
+++ irram.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python
-'''
 
+"""
+Main file for irramsh.
+irramsh is a version and package manager for iRRAM.
+Maintainer or person who modify the file to adjust the file to their environment
+should be aware of that the program has access to a file system.
+This version is a work in progress-prototype version; the project is created mainly to make
+the author's life of programming on iRRAM easier; and distributed in hope of making others' easier as well;
+there is no warranty of any result of this program; If any problem occurs during using this program,
+it is the user's fault not inspecting the file thoroughly.
+"""
 
-'''
-import sys, getopt, subprocess, errno, os
-import readline, glob
+import sys, getopt, subprocess, os, readline, glob
+
+__author__ = "sewon park"
+__copyright__ = "Copyright 2018, sewon park"
+__license__ = "MIT License"
+__status__ = "Prototype, work in progress"
+
 path = os.path.dirname(os.path.abspath(__file__))
 
 
 def exodus(msg):
     print msg + '  See "irramsh -h|--help" for further information.'
     sys.exit(1)
+
 
 def usage():
     s = 'iRRAM version and package manager.\n' \
@@ -32,6 +46,8 @@ def usage():
         '"-o <filename.cc> helloworld" creates an irram template <filename.cc>\n'\
         '"add" adds another iRRAM version to the irramsh\n'\
         '"switch <n>" switchs the iRRAM compiler to the version <n>. Try "irramsh -a" to see what versions you have\n'\
+        '"select <n>" names the registered version <n> to be the 201401 official release of iRRAM\n'\
+        '"deselect" de-select the selected official release version of iRRAM\n'\
         '"clear" clears the saved configuration. Use it when the irramsh got too messy\n\n' \
         'package managing:\n'\
         '"show" shows available packages in irramsh package repo\n'\
@@ -50,6 +66,11 @@ def complete(text, state):
 
 
 hw = '#include "iRRAM/lib.h"\n#include "iRRAM/core.h"\n#include "iRRAM.h"\nusing namespace iRRAM;\nvoid compute()\n{\ncout<<"halo world";\n}'
+CONFIGFILE = path+"/config"
+PKGCONFIGFILE = path+"/packages/pkgconfig"
+TESTFILE = path+"/test.cc"
+PKGPATH = path+"/packages"
+
 def message(iRRAMPath, INPUT, LIB, LIBPATH, OUTPUT):
 
     return "g++ -std=c++11 -g -O2 -I" + iRRAMPath + "/include " + LIBPATH + " -Xlinker -rpath -Xlinker "+iRRAMPath+"/lib "\
@@ -64,7 +85,7 @@ def load_config():
         version = 0
         locationlst = []
         location = ""
-        with open(path+'/config', 'r') as f:
+        with open(CONFIGFILE, 'r') as f:
             content = f.readlines()
         configs = [x.strip() for x in content]
         for c in configs:
@@ -74,11 +95,8 @@ def load_config():
                 locationlst.append(c.split("V="))
                 if int(c.split("V=")[0]) == version:
                     location = c.split("V=")[1]
-            elif "cleared" in c:
-                version = 0
-                locationlst = []
 
-        if version == 0 or location == []:
+        if location == []:
             raise ValueError('Configuration File Defective')
 
         return locationlst, location, version
@@ -90,31 +108,36 @@ def load_config():
 
 def add_irram():
 
-    test = open(path+'/test.cc', 'w')
+    test = open(TESTFILE, 'w')
     test.write('')
     test.write(hw)
     test.close()
 
-    verif = True
-    while verif:
+    verif = False
+    counter = 3
+    while (not verif) and counter >= 0:
 
         readline.set_completer_delims(' \t\n;')
         readline.parse_and_bind("tab: complete")
         readline.set_completer(complete)
 
-        location = raw_input("Locate iRRAM installation directory:\n")
+        location = raw_input("Locate iRRAM installation directory: /path/to/iRRAM/installed \n")
+        while location[-1] == '/':
+            location = location[:-1]
 
         try:
             subprocess.check_call(message(location, path+"/test.cc", "", "", "a.out"), shell=True)
-            verif = False
+            verif = True
 
         except:
-            print "Couldn't compile a test file. Try again!"
-            sys.exit()
+            print 'Could not compile a test file with the selected version of iRRAM.'
+            counter -= 1
 
+    if not verif:
+        exodus('The selected version of iRRAM does not work. Check whether the installed iRRAM is properly installed "locally"')
 
     num = -1
-    with open(path+'/config', 'r') as f:
+    with open(CONFIGFILE, 'r') as f:
         config = f.readlines()
 
     newconf = ""
@@ -125,12 +148,64 @@ def add_irram():
         newconf += (l)
     newconf += (str(num+1)+"V="+location+"\n")
     print newconf
-    with open(path+'/config', 'w') as f:
+    with open(CONFIGFILE, 'w') as f:
         f.write(newconf)
 
 
+
+
+def select_official(v):
+    '''
+    select among registered versions of iRRAM, the official release
+    '''
+    v = v.strip()
+    try:
+        exists = False
+        with open(CONFIGFILE, 'r') as f:
+            config = f.readlines()
+        q = []
+        for l in config:
+            if "VERSION=" in l:
+                if int(l.split("=")[1]) == int(v):
+                    q.append("VERSION=0")
+                else:
+                    q.append(l)
+            elif "0V=" in l:
+                exodus('Official release is already selected. "deselect" the official release first')
+            elif v+"V"+"=" in l:
+                q.append("0V="+l.split("=")[1])
+                exists = True
+            else:
+                q.append(l)
+        if not exists:
+            exodus('Selected version ' + v + " does not exist")
+        with open(CONFIGFILE, 'w') as f:
+            f.writelines(q)
+    except Exception as err:
+        exodus('Could not change version')
+
+def deselct_official():
+    try:
+        with open(CONFIGFILE, 'r') as f:
+            config = f.readlines()
+        q = []
+        num = 0
+        for l in config:
+            if "NUM=" in l:
+                num = int(l.split("=")[1])
+            elif "V0=" in l:
+                l = "v"+str(num+1)+"="+l.split("=")[1]
+
+            q.append(l)
+
+        with open(CONFIGFILE, 'r') as f:
+            f.writelines(q)
+    except:
+        exodus('Could not change version')
+
+
 def switch(v):
-    with open(path+'/config', 'r') as f:
+    with open(CONFIGFILE, 'r') as f:
         config = f.readlines()
     newconf = ""
     ex = True
@@ -144,24 +219,26 @@ def switch(v):
     if ex:
         raise ValueError('Selected Switching Version Does not Exist')
 
-    with open(path+'/config', 'w') as f:
+    with open(CONFIGFILE, 'w') as f:
         f.write(newconf)
 
 
 def clear():
-    config = open(path+'/config', 'w')
+    config = open(CONFIGFILE, 'w')
     config.close()
-
 
 
 '''
 PACKAGE MANAGEMENT
 '''
+
+
 def load_package_index():
     import urllib2
     response = urllib2.urlopen('https://complexity.kaist.ac.kr/_media/irramsh/irramsh-package-index.odt')
     html = response.readlines()
     return [x.strip().split(",".strip()) for x in html]
+
 
 def uninstall_package(pkgname):
 
@@ -170,6 +247,7 @@ def uninstall_package(pkgname):
         if pkgname == p[0]:
             return
     exodus('Package name ' + pkgname + ' is not installed')
+
 
 def install_package(pkgname):
 
@@ -188,8 +266,10 @@ def install_package(pkgname):
                     os.makedirs(path + '/packages/'+p[0])
                 uz = 'unzip ' + path+'/packages/'+p[0]+'.zip ' + '-d ' + path+'/packages/'+p[0]
                 subprocess.check_call(uz, shell= True)
-                with open(path + '/packages/pkgconfig', 'a') as f:
+
+                with open(PKGCONFIGFILE, 'a') as f:
                     f.write(p[0] + ", 1\n")
+
             except:
                 exodus('Installing ' + pkgname + ' failed')
             return
@@ -202,7 +282,7 @@ def deactivate_package(pkgname):
         if pkgname == p[0]:
             if int(p[1]) == 0:
                 p[1] = 1
-    with open(path+'/packages/pkgconfig', 'w') as f:
+    with open(PKGCONFIGFILE, 'w') as f:
         for p in installed:
             f.writelines(p)
 
@@ -212,29 +292,37 @@ def activate_package(pkgname):
         if pkgname == p[0]:
             if int(p[1]) == 0:
                 p[1] = 1
-    with open(path+'/packages/pkgconfig', 'w') as f:
+    with open(PKGCONFIGFILE, 'w') as f:
         for p in installed:
             f.writelines(p)
 
 
 def load_package_config():
-    with open(path+'/packages/pkgconfig', 'r') as f:
+    with open(PKGCONFIGFILE, 'r') as f:
         pkgs = f.readlines()
     pkgs = [x.strip().split(",".strip()) for x in pkgs]
     return pkgs
 
 
-def load_package_paths():
+def load_package_paths(version):
     pkgs = load_package_config()
     libpath = " "
     s = " "
     for p in pkgs:
         libpath += "-I "+path+"/packages/"+p[0]+" "
-        with open(path+"/packages/"+p[0] +"/irramsh", 'r') as f:
+        if int(version) == 0:
+            pt = path+"/packages/"+p[0] +"/irramsh_official"
+        else:
+            pt = path+"/packages/"+p[0] +"/irramsh"
+
+        with open(pt, 'r') as f:
             srcs = f.readlines()
             for l in srcs:
                 s+=path+"/packages/"+p[0]+"/"+(l.strip())+" "
     return s, libpath
+
+
+
 
 '''
 Initializer: creates
@@ -244,13 +332,13 @@ Initializer: creates
 import os
 def initialize():
     newconfig = False
-    if not os.path.exists(path+'/packages'):
-        os.makedirs(path+'/packages')
-    if not os.path.exists(path+'/packages/pkgconfig'):
-        f = open(path+'/packages/pkgconfig', 'w')
+    if not os.path.exists(PKGPATH):
+        os.makedirs(PKGPATH)
+    if not os.path.exists(PKGCONFIGFILE):
+        f = open(PKGCONFIGFILE, 'w')
         f.close()
-    if not os.path.exists(path+'/config'):
-        config = open(path+'/config', 'w')
+    if not os.path.exists(CONFIGFILE):
+        config = open(CONFIGFILE, 'w')
         config.write('NUM=0\nVERSION=0\n')
         config.close()
         newconfig = True
@@ -259,8 +347,7 @@ def initialize():
             add_irram()
             switch(1)
         except:
-            print "cannot create a configuration file"
-            sys.exit()
+            exodus("cannot create a configuration file")
 
 
 
@@ -297,15 +384,22 @@ def main(argv):
 
         elif opt in ("-a", "--all"):
             for l in locationlst:
-                print "version " + l[0] + " @ " + l[1]
+                if int(l[0]) == 0:
+                    print "version " + l[0] + " @ " + l[1] +" (official release) "
+                else:
+                    print "version " + l[0] + " @ " + l[1]
             sys.exit()
 
         elif opt in ("-v", "--version"):
-            print "Using version "+ str(version) +" installed in " + location
+            if version == 0:
+                print "Using (official release) version "+ str(version) +" installed in " + location
+            else:
+                print "Using version "+ str(version) +" installed in " + location
+
             sys.exit()
 
         elif opt in ("-f", "--force"):
-            Force = True
+            FORCE = True
 
         elif opt in ("-o", "--output"):
             OUTPUT = arg
@@ -334,7 +428,7 @@ def main(argv):
             v = int(args[1])
             switch(v)
         except:
-            raise
+            exodus('Could not change the version')
         sys.exit()
 
     elif mainarg == "change":
@@ -380,6 +474,14 @@ def main(argv):
                 print p[0]
         sys.exit()
 
+    elif mainarg == "select":
+        select_official(args[1])
+        sys.exit()
+
+    elif mainarg == "deselect":
+        deselct_official()
+        sys.exit()
+
 
     '''
     Compile is by default. 
@@ -388,16 +490,20 @@ def main(argv):
     '''
     if os.path.exists(path+'/'+OUTPUT):
         if not FORCE:
-            raise ValueError('File ' + OUTPUT + ' already exists')
+            exodus('File ' + OUTPUT + ' already exists')
     for f in args:
         INPUT += f + " "
-    s, l = load_package_paths()
+    s, l = load_package_paths(version)
 
 
     '''
     Compile the iRRAM file.. 
     '''
-    subprocess.check_call(message(location, INPUT, s, l, OUTPUT), shell=True)
+    try:
+        subprocess.check_call(message(location, INPUT, s, l, OUTPUT), shell=True)
+    except subprocess.CalledProcessError as err:
+        # print err.output
+        exodus('\n[irramsh] compile failed')
 
 if __name__ == "__main__":
 
